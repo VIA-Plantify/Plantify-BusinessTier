0d82fe9789e28d3fc4058fd29bfd992071c29dac
patri <patrik.golian888@gmail.com>
2026-04-22 11:56:00 +0000

    added plant entity (it has commented out some parameters, because I am waiting for Teo's classes)

--- Shared/Entities/Plant/Plant.cs
+++ Shared/Entities/Plant/Plant.cs
@@ -1,7 +1,39 @@
 namespace Entities.Plant;
 
 public class Plant
-{
-    public ITemperature Temperature { get; set; }
+{ 
+    public int Id { get; set; }
+    public string Name { get; set; } = string.Empty;
+    public User Owner { get; set; }
+
+    public ITemperature Temperature { get; set; } = new Temperature();
+    public IAirHumidity AirHumidity { get; set; } = new AirHumidity();
+    public IWaterLevel WaterLevel { get; set; } = new WaterLevel();
+    public IWaterIntake WaterIntake { get; set; }  = new WaterIntake();
+    //public IHumidity SoilHumidity { get; set; } = new SoilHumidity();
+    //public ILightSensitivity LightSensitivity { get; set; } = new LightSensitivity();
     
+    public double OptimalTemperature 
+    { 
+        get => Temperature.OptimalTemperature; 
+        set => Temperature.OptimalTemperature = value; 
+    }
+
+    public double OptimalAirHumidity 
+    { 
+        get => AirHumidity.OptimalAirHumidity; 
+        set => AirHumidity.OptimalAirHumidity = value; 
+    } 
+    // public double OptimalSoilHumidity 
+    // { 
+    //     get => SoilHumidity.OptimalSoilHumidity; 
+    //     set => SoilHumidity.OptimalSoilHumidity = value; 
+    // } 
+    // public double OptimalLightSensitivity 
+    // { 
+    //     get => LightSensitivity .OptimalLightSensitivity; 
+    //     set => LightSensitivity .OptimalLightSensitivity = value; 
+    // } 
+    public int? WaterLevelPercentage => WaterLevel.WaterLevelPercentage;
+    public double? LastWaterVolume => WaterIntake.Volume;
 }
