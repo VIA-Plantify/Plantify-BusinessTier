6a1d05daa1d496863839a50d551e2b004a5f40bb
MarioIliescu <355142@via.dk>
2026-04-22 08:57:27 +0000

    Moved Temperature logic from plant to temperature

--- Shared/Entities/Plant/Plant.cs
+++ Shared/Entities/Plant/Plant.cs
@@ -3,44 +3,5 @@ namespace Entities.Plant;
 public class Plant
 {
     public ITemperature Temperature { get; set; }
-    public TemperatureScale currentScale { get; set; } = TemperatureScale.C;
-
-    public async Task<ITemperature> ConvertTemperature(TemperatureScale scale)
-    {
-        if (currentScale == scale)
-        {
-            return await Task.FromResult(Temperature);
-        }
-
-        Temperature.OptimalTemperature = ConvertScale(scale, Temperature.OptimalTemperature);
-        if (Temperature.CurrentTemperature is not null)
-        {
-            Temperature.CurrentTemperature = ConvertScale(scale, Temperature.CurrentTemperature);
-        }
-
-        for (int i = 0; i < Temperature?.PastTemperatureReadings.Count; i++)
-        {
-            Temperature.PastTemperatureReadings[i] = ConvertScale(scale, Temperature.PastTemperatureReadings[i]);
-        }
-
-        currentScale = scale;
-        return await Task.FromResult(Temperature) ?? throw new InvalidOperationException("Something went wrong with the Temperature");
-    }
-
-    private double ConvertScale(TemperatureScale scale, double? value)
-    {
-        if (value is null)
-        {
-            throw new ArgumentNullException(nameof(value), "Value is not given");
-        }
-        switch (scale)
-        {
-            case TemperatureScale.F:
-                return (double)((value - 32) / 1.8);
-            case TemperatureScale.C:
-                return (double)(value * 1.8 + 32);
-            default:
-                throw new ArgumentOutOfRangeException(nameof(scale), scale, "Scale not supported");
-        }
-    }
+    
 }
--- Shared/Entities/Plant/Temperature.cs
+++ Shared/Entities/Plant/Temperature.cs
@@ -4,11 +4,47 @@ public class Temperature : ITemperature
 {
     
     public double? CurrentTemperature { get; set; }
-    public IList<double?> PastTemperatureReadings { get; set; }
+    public IList<double?> PastTemperatureReadings { get; set; } = new List<double?>();
     public double OptimalTemperature { get; set; }
     
-    public Temperature()
+    public TemperatureScale currentScale { get; set; } = TemperatureScale.C;
+    
+    public async Task<ITemperature> ConvertTemperature(TemperatureScale scale)
     {
-        PastTemperatureReadings = new List<double?>();
+        if (currentScale == scale)
+        {
+            return await Task.FromResult(this);
+        }
+
+        this.OptimalTemperature = ConvertScale(scale, this.OptimalTemperature);
+        if (this.CurrentTemperature is not null)
+        {
+            this.CurrentTemperature = ConvertScale(scale, this.CurrentTemperature);
+        }
+
+        for (int i = 0; i < this?.PastTemperatureReadings.Count; i++)
+        {
+            this.PastTemperatureReadings[i] = ConvertScale(scale, this.PastTemperatureReadings[i]);
+        }
+
+        currentScale = scale;
+        return await Task.FromResult(this) ?? throw new InvalidOperationException("Something went wrong with the Temperature");
+    }
+
+    private double ConvertScale(TemperatureScale scale, double? value)
+    {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value), "Value is not given");
+        }
+        switch (scale)
+        {
+            case TemperatureScale.F:
+                return (double)((value - 32) / 1.8);
+            case TemperatureScale.C:
+                return (double)(value * 1.8 + 32);
+            default:
+                throw new ArgumentOutOfRangeException(nameof(scale), scale, "Scale not supported");
+        }
     }
 }
