dc618c0ed6a7a7ff4511c75c1ac67c38dec5cef9
MarioIliescu <355142@via.dk>
2026-04-22 08:53:37 +0000

    Added nullable values

--- Shared/Entities/Plant/Plant.cs
+++ Shared/Entities/Plant/Plant.cs
@@ -3,7 +3,7 @@ namespace Entities.Plant;
 public class Plant
 {
     public ITemperature Temperature { get; set; }
-    public TemperatureScale currentScale { get; set; }
+    public TemperatureScale currentScale { get; set; } = TemperatureScale.C;
 
     public async Task<ITemperature> ConvertTemperature(TemperatureScale scale)
     {
@@ -11,24 +11,34 @@ public class Plant
         {
             return await Task.FromResult(Temperature);
         }
+
         Temperature.OptimalTemperature = ConvertScale(scale, Temperature.OptimalTemperature);
-        Temperature.CurrentTemperature = ConvertScale(scale, Temperature.CurrentTemperature);
-        for (int i = 0; i < Temperature.PastTemperatureReadings.Count; i++)
+        if (Temperature.CurrentTemperature is not null)
+        {
+            Temperature.CurrentTemperature = ConvertScale(scale, Temperature.CurrentTemperature);
+        }
+
+        for (int i = 0; i < Temperature?.PastTemperatureReadings.Count; i++)
         {
             Temperature.PastTemperatureReadings[i] = ConvertScale(scale, Temperature.PastTemperatureReadings[i]);
         }
+
         currentScale = scale;
-        return await Task.FromResult(Temperature);
+        return await Task.FromResult(Temperature) ?? throw new InvalidOperationException("Something went wrong with the Temperature");
     }
 
-    private double ConvertScale(TemperatureScale scale, double value )
+    private double ConvertScale(TemperatureScale scale, double? value)
     {
+        if (value is null)
+        {
+            throw new ArgumentNullException(nameof(value), "Value is not given");
+        }
         switch (scale)
         {
             case TemperatureScale.F:
-                return (value - 32) / 1.8;
+                return (double)((value - 32) / 1.8);
             case TemperatureScale.C:
-                return value * 1.8 + 32;
+                return (double)(value * 1.8 + 32);
             default:
                 throw new ArgumentOutOfRangeException(nameof(scale), scale, "Scale not supported");
         }
--- Shared/Entities/Plant/Temperature.cs
+++ Shared/Entities/Plant/Temperature.cs
@@ -3,12 +3,12 @@ namespace Entities.Plant;
 public class Temperature : ITemperature
 {
     
-    public double CurrentTemperature { get; set; }
-    public IList<double> PastTemperatureReadings { get; set; }
+    public double? CurrentTemperature { get; set; }
+    public IList<double?> PastTemperatureReadings { get; set; }
     public double OptimalTemperature { get; set; }
     
     public Temperature()
     {
-        PastTemperatureReadings = new List<double>();
+        PastTemperatureReadings = new List<double?>();
     }
 }
