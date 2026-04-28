34fa2463b7317a40c0627ad9e53f29d8d1ac866d
MarioIliescu <355142@via.dk>
2026-04-28 16:56:45 +0000

    Made cors easier to use when in development env

--- WebApi/WebAPI/Program.cs
+++ WebApi/WebAPI/Program.cs
@@ -24,11 +24,22 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("Frontend", policy =>
     {
-        policy
-            .WithOrigins(allowedOrigins)
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .AllowCredentials();
+        if (builder.Environment.IsProduction())
+        {
+            policy
+                .WithOrigins(allowedOrigins)
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .AllowCredentials();
+        }
+        else
+        {
+            policy
+                .AllowAnyMethod()
+                .AllowAnyHeader()
+                .SetIsOriginAllowed(_ => true)
+                .AllowCredentials();
+        }
     });
 });
 
@@ -107,8 +118,10 @@ builder.Services.AddScoped<IPlantService, PlantService>();
 
 
 var app = builder.Build();
-
-app.UseHttpsRedirection();
+if (!app.Environment.IsDevelopment())
+{
+    app.UseHttpsRedirection();
+}
 
 app.UseRouting();
 app.UseCors("Frontend");
